40a6eac7a65f38556cd43853a93faf148fc24044
Adam Rudziński <devel@arf.net.pl>
2018-07-18 09:52:31 +0200

    added missing copyright notes

--- include/PagedMemory.hpp
+++ include/PagedMemory.hpp
@@ -1,3 +1,11 @@
+/*
+PagedMemory.hpp
+Author: Adam Rudziński, devel@arf.net.pl
+Copyright: Adam Rudziński, 2018
+This is free software, licensed under GNU GPLv3 license.
+This software comes with ABSOLUTELY NO WARRANTY, USE AT YOUR OWN RISK!
+*/
+
 #ifndef __PAGEDMEMORY_HPP
 #define __PAGEDMEMORY_HPP
 
--- include/linux/PIDFile.hpp
+++ include/linux/PIDFile.hpp
@@ -1,3 +1,11 @@
+/*
+PIDFile.hpp
+Author: Adam Rudziński, devel@arf.net.pl
+Copyright: Adam Rudziński, 2018
+This is free software, licensed under GNU GPLv3 license.
+This software comes with ABSOLUTELY NO WARRANTY, USE AT YOUR OWN RISK!
+*/
+
 #ifndef __LINUX_PIDFILE_HPP
 #define __LINUX_PIDFILE_HPP
 
