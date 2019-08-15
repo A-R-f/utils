4372ea0292ffe4484ae8cb01f94e52ea38b4d156
Adam Rudziński <devel@arf.net.pl>
2019-08-15 22:28:02 +0200

    Signal.hpp: use separate struct sigaction in constructor to avoid problems with restricted arguments

--- include/linux/Signal.hpp
+++ include/linux/Signal.hpp
@@ -1,7 +1,7 @@
 /*
 Signal.hpp
 Author: Adam Rudziński, devel@arf.net.pl
-Copyright: Adam Rudziński, 2017
+Copyright: Adam Rudziński, 2017-2019
 This is free software, licensed under GNU GPLv3 license.
 This software comes with ABSOLUTELY NO WARRANTY, USE AT YOUR OWN RISK!
 */
@@ -9,6 +9,7 @@ This software comes with ABSOLUTELY NO WARRANTY, USE AT YOUR OWN RISK!
 #ifndef __LINUX_SIGNAL_HPP
 #define __LINUX_SIGNAL_HPP
 
+#include <cstring>
 #include <signal.h>
 
 class Signal {
@@ -21,9 +22,10 @@ public:
 	Signal(const int _signo, sighandler_t _handler) :
 		signo(_signo)
 	{
-		for( char* p = (char*)&sa + sizeof(sa) ; p != (char*)&sa ; ) { *--p = 0; }
-		sa.sa_handler = _handler;
-		sigaction(signo, &sa, &sa);
+		struct sigaction new_sa;
+		bzero(&new_sa, sizeof(new_sa));
+		new_sa.sa_handler = _handler;
+		sigaction(signo, &new_sa, &sa);
 	}
 
 	~Signal() { sigaction(signo, &sa, NULL); }
